6e058037dd1798057573d06fe06a416a7291ca3f
Sarah <Ms.sarahelizabethb@gmail.com>
2018-04-12 18:27:00 -0400

    first commit

---
+++ Lab 1 part 2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hereWeGoAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool error = false;
+            DateTime endTime = new DateTime(), startTime = new DateTime();  
+            do
+            { 
+                if (error)
+                {
+                    // if used enters wrong date format, it will therefore be concidered false and the console will print " wrong date format,try again"
+                    Console.WriteLine("oops, that does not look like the right date format, try again! (mm-dd,yyyy");
+                }
+
+				error = false;
+
+                // asking the user to enter the first date of choice
+                Console.WriteLine("Please pick your first date(mm-dd-yyyy with the dashes included): ");
+                try
+                {
+                    startTime = DateTime.ParseExact(Console.ReadLine(), "mm-dd-yyyy", CultureInfo.InvariantCulture);
+                }
+                catch (Exception)
+                {
+                    error = true;
+                }
+            } while (error);
+            do
+            {
+                if (error)
+					// if the user puts in the wrong date format it will print "try again with the correct date format
+                    Console.WriteLine("That doest quite look like the right format, Try again! (mm-dd,yyyy)!");
+					
+                error = false;
+                Console.WriteLine("Please pick a second date(mm-dd-yyyy with the dashes included): ");
+                try
+                {
+                    endTime = DateTime.ParseExact(Console.ReadLine(), "mm-dd-yyyy", CultureInfo.InvariantCulture);
+                }
+                catch (Exception)
+                {
+                    error = true;
+                }
+            } while (error);
+
+            TimeSpan span = endTime.Subtract(startTime);
+            int days = span.Days;
+            if (days < 0)
+                days *= -1; 
+            // based from the correct date format of bothe dates, the console will print the calculations for years, months, days minutes and seconds
+            String spanMsg = string.Format("{0} years, {1} months, {2} days, {3} hours, and {4} minutes",
+                days / 365, days / 12, days, days * 24, days * 24 * 60, days * 24 * 60);
+            Console.WriteLine(spanMsg);
+            Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
---
+++ lab 1 part 1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letsDoThisAgain
+{
+    class digit_3_Compare
+    
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Would you like to check to see if two 3 digit numbers sums are equal to the same total. yes or no ?... Lets Go! ");
+            var wouldYouLikeToContinue = Console.ReadLine();
+
+            if (wouldYouLikeToContinue != null && wouldYouLikeToContinue.Equals("yes", StringComparison.CurrentCultureIgnoreCase))
+            {
+                var num1 = GetIntFromUser("Please pick your first three digit whole number");
+                var num2 = GetIntFromUser("Please enter a three digit whole number");
+
+                Console.WriteLine(Compare(num1, num2) ? "True" : "False");
+            }
+            else
+            {
+                Console.WriteLine("wrong Answer, Goodbye");
+            }
+
+            // waits for the user to press a key to exit the app
+            Console.ReadKey();
+        }
+
+        // <piece by piece>
+        // user enters first three digit number, and  if they say "no" then  console prints "wrong answer, goodbye"
+        public static int GetIntFromUser(string msg)
+        {
+            while (true)
+            {
+                Console.WriteLine(msg);
+                var valFromUser = Console.ReadLine()?.Trim();
+
+                if (valFromUser != null)
+                {
+                    int result;
+                    if (int.TryParse(valFromUser.Trim(), out result) && valFromUser.Length == 3)
+                    {
+                        return result;
+                    }
+                    // if user answers "no" then the console prints "Goodbye :( "
+                    if (valFromUser.Equals("no", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        Console.WriteLine("Goodbye :( ");
+                        Environment.Exit(0);
+                    }
+                } // if the user enters more or less than 3 digits the cons
+                Console.WriteLine("thats not the right number of digits. please try again.");
+            }
+        }
+
+        public static bool Compare(int a, int b)
+        { // use modulus to devide and find the remainder (singles out one digit)
+            var lastDigitA = a % 10;
+            var lastDigitB = b % 10;
+            var sumStatic = lastDigitA + lastDigitB;
+
+            do
+            {
+                lastDigitA = a % 10;
+                lastDigitB = b % 10;
+                a = a / 10;
+                b = b / 10;
+                // you will be comparing the last digit from each 3 digit number
+                var sumCompare = lastDigitA + lastDigitB;
+
+                if (sumCompare != sumStatic)
+                {
+                    return false;
+                }
+            } while (b != 0);
+            return true;
+        }
+    }
+}
